b9c932df543cb9865b0d38a496c0da63fd403ae2
Michael Neu <git@michaeln.eu>
2018-03-07 19:52:13 +0100

    remove unused admin class in user overview

--- app/client/views/admin/users/adminUsersCtrl.js
+++ app/client/views/admin/users/adminUsersCtrl.js
@@ -234,9 +234,6 @@ angular.module('reg')
       }
 
       $scope.rowClass = function(user) {
-        if (user.admin){
-          return 'admin';
-        }
         if (user.status.confirmed) {
           return 'positive';
         }
