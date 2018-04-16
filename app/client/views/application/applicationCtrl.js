c5bcf34dc5c0dfbadb6ee5a257845de903cd2813
Michael Neu <git@michaeln.eu>
2018-04-16 21:57:04 +0200

    fix typo in status call

--- app/client/views/application/applicationCtrl.js
+++ app/client/views/application/applicationCtrl.js
@@ -46,7 +46,7 @@ angular.module('reg')
       populateNations();
       _setupForm();
 
-      $scope.regIsClosed = Date.now() > Settings.data.timeClose || $scope.user.status.status.admitted;
+      $scope.regIsClosed = Date.now() > Settings.data.timeClose || ($scope.user.status && $scope.user.status.admitted);
 
       /**
        * TODO: JANK WARNING
